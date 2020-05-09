eaaefe56972980d4e8a80a6674ecc6660e7f52e7
Brent Labasan <BrentLabasan@gmail.com>
2020-05-08 21:24:52 -0700

    Updates

---
+++ precache-manifest.0fe574bce61d92c5ad96f995d5430b07.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6eb5bdb872f7fe9f5071b24cc7971d4",
+    "url": "/NLNRF-Redux/index.html"
+  },
+  {
+    "revision": "df257075ccbb823d006d",
+    "url": "/NLNRF-Redux/static/css/main.419131ca.chunk.css"
+  },
+  {
+    "revision": "9864c884306c18241622",
+    "url": "/NLNRF-Redux/static/js/2.e4ef69ce.chunk.js"
+  },
+  {
+    "revision": "666b17790262826dc1d825807078f9dc",
+    "url": "/NLNRF-Redux/static/js/2.e4ef69ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df257075ccbb823d006d",
+    "url": "/NLNRF-Redux/static/js/main.9455718d.chunk.js"
+  },
+  {
+    "revision": "04a29222447658eec590",
+    "url": "/NLNRF-Redux/static/js/runtime-main.b0a15c8f.js"
+  },
+  {
+    "revision": "da690e49ec9199a55b0b0a6260536d14",
+    "url": "/NLNRF-Redux/static/media/poopandneedles.da690e49.png"
+  },
+  {
+    "revision": "ffd5e84b4810fc36f85dd46f2f5b570a",
+    "url": "/NLNRF-Redux/static/media/pulsing3.ffd5e84b.gif"
+  },
+  {
+    "revision": "9b488c89852ba0132e36581d9e212cca",
+    "url": "/NLNRF-Redux/static/media/wut.9b488c89.gif"
+  },
+  {
+    "revision": "8685941e80afa5c23a94e07808a4b6e5",
+    "url": "/NLNRF-Redux/static/media/xu.8685941e.png"
+  }
+]);
